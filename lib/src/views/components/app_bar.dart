9b6d95f23421a565a7cd43e962edc3a90528c83c
Marcos Gabriel de Mesquita Mauricio <mgmesquitamaurcio@gmail.com>
2024-03-10 02:07:47 -0300

    buttons implemented

--- lib/src/views/components/app_bar.dart
+++ lib/src/views/components/app_bar.dart
@@ -1,86 +1,117 @@
-import 'package:d20_project/src/views/components/bottom_sheet_input.dart';
-import 'package:d20_project/styles/colors_app.dart';
-import 'package:d20_project/styles/text_styles.dart';
+// ignore_for_file: public_member_api_docs, sort_constructors_first
 import 'package:flutter/material.dart';
 
-class BarraApp extends StatelessWidget implements PreferredSizeWidget {
-  const BarraApp({super.key});
+import 'package:d20_project/src/views/components/bottom_sheet.dart';
+import 'package:d20_project/src/models/players.dart';
+import 'package:d20_project/styles/colors_app.dart';
+import 'package:d20_project/styles/text_styles.dart';
+
+class BarraApp extends StatefulWidget implements PreferredSizeWidget {
+  final ValueNotifier<int> refreshNotifier;
+  final bool isSelectionMode;
+  final IconData icon;
+  final Function(IconData) onIconChanged;
+
+  const BarraApp({
+    Key? key,
+    required this.refreshNotifier,
+    required this.icon,
+    this.isSelectionMode = false, 
+    required this.onIconChanged,
+  }) : super(key: key);
 
   @override
-  Size get preferredSize => const Size.fromHeight(60);
+  State<BarraApp> createState() => _BarraAppState();
+  
+  @override
+  Size get preferredSize => const Size.fromHeight(60.0);
+}
+
+class _BarraAppState extends State<BarraApp> {
+  
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 18),
-      child: AppBar(
-        title: Text("INICIATIVA", style: TextStyles.instance.regular),
-        centerTitle: false,
-        actions: [
-          IconButton(
-
-            icon: const Icon(Icons.add),
-            onPressed: () { _displayBottomSheet(context); },
-          ),
-          IconButton(
-            onPressed: () {},
-            icon: const Icon(Icons.sort)
-          ),
-        ],
-      ),
-    );
-  }
-}
-
-Future _displayBottomSheet(BuildContext context){
-  return showModalBottomSheet(
-    context: context,
-    isScrollControlled: true,
-    barrierColor: Colors.black.withOpacity(0.5),
-    backgroundColor: ColorsApp.instance.primaryColor,
-    shape: const RoundedRectangleBorder(borderRadius: BorderRadius.vertical(top: Radius.circular(30))),
-    builder: (BuildContext context) {
-      return Container(
-        padding: EdgeInsets.only(bottom: MediaQuery.of(context).viewInsets.bottom),
-        child: Padding (
-          padding: const EdgeInsets.all(16),
-          child: Form (
-            child: Padding(
-              padding: const EdgeInsets.symmetric(vertical: 15, horizontal: 18),
-              child: Column(
-                mainAxisSize: MainAxisSize.min,
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text("NOVA INICIATIVA", style: TextStyles.instance.regular.copyWith(fontSize: 20)),
-                  const InputSheet(campoTexto: "NOME", entradaExemplo: "Kuth"),
-                  const InputSheet(campoTexto: "CLASSE", entradaExemplo: "Mago"),
-                  const InputSheet(campoTexto: "INICIATIVA", entradaExemplo: "12"),
-                  Padding(
-                    padding: const EdgeInsets.only(top: 30),
-                    child: ElevatedButton(
-                      onPressed: () {}, 
-                      style: ElevatedButton.styleFrom(
-                        backgroundColor: ColorsApp.instance.secondaryColor,
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(5),
-                        ),
-                      ),
-                      child: SizedBox(
-                        width: double.infinity,
-                        height: 65,
-                        child: Center(
-                          child: Text("FEITO", style: TextStyles.instance.regular),
-                        ),
-                      ),
-                    ),
-                  )
-                ],
+    return AppBar(
+      title: !widget.isSelectionMode 
+        ? Padding(
+          padding: const EdgeInsets.only(left: 18),
+          child: Text("Iniciativas", style: TextStyles.instance.regular),
+        ) 
+        : Padding(
+          padding: const EdgeInsets.only(left: 9),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              IconButton(
+                onPressed: () {
+                  setState(() {
+                    if (widget.icon == Icons.radio_button_off) {
+                      for (var element in playersList) {
+                        element.isSelected = true;
+                      }
+                      widget.onIconChanged(Icons.radio_button_on);
+                    }
+                    else {
+                      for (var element in playersList) {
+                        element.isSelected = false;
+                      }
+                      widget.onIconChanged(Icons.radio_button_off);
+                    }
+                    widget.refreshNotifier.value++;
+                  });
+                },
+                icon: Icon(widget.icon)
               ),
+              Padding(
+                padding: const EdgeInsets.only(left: 8),
+                child: Text("Todos", style: TextStyles.instance.regular),
+              ),
+            ],
+          ),
+        ),
+      centerTitle: false,
+      actions: [
+        IconButton(
+          tooltip: "Adicionar nova iniciativa",
+          icon: const Icon(Icons.add),
+          onPressed: ()  async { displayBottomSheet(context, () => widget.refreshNotifier.value++);},
+        ),
+        Padding(
+          padding: const EdgeInsets.only(right: 18),
+          child: IconButton(
+            tooltip: "Ordenar iniciativas",
+            onPressed: () {},
+            icon: PopupMenuButton<String>(
+              shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+              color: ColorsApp.instance.secondaryColor,
+              icon: const Icon(Icons.sort, color: Colors.white,),
+              onSelected: (value) {
+                setState(() {
+                  if (value == "Nome") {
+                    playersList.sort((a, b) => a.playerName.compareTo(b.playerName));
+                  } else if (value == "Decrescente") {
+                    playersList.sort((a, b) => b.initiatives.compareTo(a.initiatives));
+                  } else if (value == "Crescente") {
+                    playersList.sort((a, b) => a.initiatives.compareTo(b.initiatives));
+                  } else {
+                    playersList.sort((a, b) => a.playerClass.compareTo(b.playerClass));
+                  }
+                  widget.refreshNotifier.value++;
+                });
+              },
+              itemBuilder: (BuildContext context) {
+                return ['Nome', 'Decrescente', "Crescente", 'Classe'].map((String choice) {
+                  return PopupMenuItem<String>(
+                    value: choice,
+                    child: Text(choice, style: TextStyles.instance.regular,),
+                  );
+                }).toList();
+              },
             ),
           ),
         ),
-      );
-    }
-  );
+      ],
+    );
+  }
 }
